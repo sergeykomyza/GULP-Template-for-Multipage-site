0691083646b357037fb8ecfedd69a3eff162be5d
Сергей Комыза <s.komiza@yandex.ru>
2023-05-30 22:12:00 +0300

    v2.0

--- build/js/main.js
+++ build/js/main.js
@@ -5,26 +5,26 @@
 // }
 
 
-// ================================================== ПРОКРУТКА, ШАПКА
-document.addEventListener('DOMContentLoaded', function () {
-    // СКРОЛЛ К НУЖНОЙ СЕКЦИИ ПО КЛИКУ НА ПУНКТАХ МЕНЮ
-    $('.menu__link').click(function () {
-        var scroll_elem = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(scroll_elem).offset().top
-        }, 1000);
-    });
-    // ДОБАВЛЯЕМ АКТИВНЫЙ КЛАСС ШАПКЕ
-    function headerActiveToggle() {
-        const scrollSize = window.pageYOffset
-        scrollSize > 1 ? header.classList.add('active') : header.classList.remove('active')
-    }
-    window.addEventListener('load', headerActiveToggle) // ПРИ ПЕРЕЗАГРУЗКЕ СТРАНИЦЫ ЕСЛИ СТРАНИЦА УЖЕ ПРОСКРОЛЛЕНА
-    window.addEventListener('scroll', headerActiveToggle) // ПРИ СКРОЛЛЕ
-});
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ПРОКРУТКА, ШАПКА
+// document.addEventListener('DOMContentLoaded', function () {
+//     // СКРОЛЛ К НУЖНОЙ СЕКЦИИ ПО КЛИКУ НА ПУНКТАХ МЕНЮ
+//     $('.menu__link').click(function () {
+//         var scroll_elem = $(this).attr('href');
+//         $('html, body').animate({
+//             scrollTop: $(scroll_elem).offset().top
+//         }, 1000);
+//     });
+//     // ДОБАВЛЯЕМ АКТИВНЫЙ КЛАСС ШАПКЕ
+//     function headerActiveToggle() {
+//         const scrollSize = window.pageYOffset
+//         scrollSize > 1 ? header.classList.add('active') : header.classList.remove('active')
+//     }
+//     window.addEventListener('load', headerActiveToggle) // ПРИ ПЕРЕЗАГРУЗКЕ СТРАНИЦЫ ЕСЛИ СТРАНИЦА УЖЕ ПРОСКРОЛЛЕНА
+//     window.addEventListener('scroll', headerActiveToggle) // ПРИ СКРОЛЛЕ
+// });
 
-// ================================================== МАСКА ДЛЯ ИНПУТОВ (https://github.com/RobinHerbots/Inputmask)
-$(document).ready(function () {
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ МАСКА ДЛЯ ИНПУТОВ (https://github.com/RobinHerbots/Inputmask)
+const inputMask = () => {
     $(".js-maskPhone").inputmask({
         mask: "+7 999 999 99 99",
         clearIncomplete: true
@@ -49,78 +49,37 @@ $(document).ready(function () {
         clearIncomplete: true,
         'placeholder': 'dd/mm/yyyy'
     });
-});
+}
 
-// ================================================== СЛАЙДЕР SLICK (https://kenwheeler.github.io/slick/)
-document.addEventListener('DOMContentLoaded', function () {
-    $('.slider').slick({
-        fade: false,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        arrows: true,
-        dots: true,
-        // prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-        // nextArrow: '<button type="button" class="slick-next">Next</button>',
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ СЛАЙДЕР SWIPER (https://swiperjs.com/get-started) 
+const sliders = () => {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+    
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
     });
-    // пользовательская навигация
-    // var dot = $(".dots__item");
-    // $('.slider').on("beforeChange", function (event, slick, currentSlide, nextSlide) {
-    //     dot.removeClass("dots__item--active").eq(nextSlide).addClass("dots__item--active")
-    // });
-    // dot.on("click", function () {
-    //     var i = dot.index(this);
-    //     $('.slider').slick("slickGoTo", i)
-    // });
-    // $(".prev").on("click", function () {
-    //     $('.slider').slick("slickPrev")
-    // });
-    // $(".next").on("click", function () {
-    //     $('.slider').slick("slickNext")
-    // });
 
-});
-// ================================================== СЛАЙДЕР SWIPER (https://swiperjs.com/get-started) 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+}
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ КАРТА, ОТЛОЖЕННАЯ ЗАГРУЗКА (ЧТОБЫ УЛУЧШИТЬ ПОКАЗАТЕЛИ - PageSpeed Insights)
+const map = () => {
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
-// ================================================== 
-// ================================================== 
-// ================================================== 
-// ================================================== 
-// ================================================== 
-// ================================================== 
-// ================================================== 
-// ================================================== 
-
-// ================================================== КАРТА, ОТЛОЖЕННАЯ ЗАГРУЗКА (ЧТОБЫ УЛУЧШИТЬ ПОКАЗАТЕЛИ - PageSpeed Insights)
-document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function() {
         var headID = document.getElementsByTagName("body")[0];         
         var newScript = document.createElement('script');
@@ -138,7 +97,6 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         myGeoObject = new ymaps.GeoObject({
-
             geometry: {
                 type: "Point"
             },
@@ -162,4 +120,50 @@ document.addEventListener('DOMContentLoaded', function () {
             myMap.behaviors.disable('drag');
         }
     }, 4000);
-});
+
+}
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/* иногда карта не загружается таким образом (например в битриксе)
+ тогда надо сделать обращение к ней как это указано в документации, через ymaps.ready - https://yandex.ru/dev/maps/jsapi/doc/2.1/quick-start/index.html?from=techmapsmain
+
+ymaps.ready(init);
+
+function init(){
+
+    var myMap = new ymaps.Map("map", {
+        center: [56.745981, 37.179787],
+        zoom: 13,
+        controls: ['smallMapDefaultSet']
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+
+    myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point"
+        },
+    });
+    myMap.geoObjects
+        .add(myGeoObject)
+        .add(new ymaps.Placemark([56.745981, 37.179787], {
+            balloonContent: '<strong></strong>',
+            iconCaption: 'М.О., г. Королев, ул. Ленина 12'
+        }, {
+            preset: 'islands#blueCircleDotIconWithCaption',
+            iconCaptionMaxWidth: '200'
+        }));
+
+    myMap.setType('yandex#publicMap');
+    // отключаем масштабирование скроллом       
+    myMap.behaviors.disable('scrollZoom');
+    // на мобильных устройствах... (проверяем по userAgent браузера)
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        //... отключаем перетаскивание карты
+        myMap.behaviors.disable('drag');
+    }
+        
+}
+*/
